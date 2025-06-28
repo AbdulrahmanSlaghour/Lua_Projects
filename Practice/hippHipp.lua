662070c297c9a55febb9d27d8f2d1119370b7d95
AbdulrahmanSlaghour <abduslaghour@gmail.com>
2025-06-28 23:18:59 +0300

    Create hippHipp.lua

---
+++ Practice/hippHipp.lua
@@ -0,0 +1,13 @@
+local name = io.read()
+
+local age = tonumber(io.read())
+
+for i = 1, age do
+    print("Hipp hipp hurra, " .. name .."!")
+endlocal name = io.read()
+
+local age = tonumber(io.read())
+
+for i = 1, age do
+    print("Hipp hipp hurra, " .. name .."!")
+end
